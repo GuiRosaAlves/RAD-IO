12a2d32470e4b409c6dc847879daa7a0d475820d
TheMoonestOne <the.moonest.one@gmail.com>
2018-01-28 08:34:02 -0200

    Novo recurso que altera speed, fase 11 refeita

---
+++ Assets/Scripts/Objects/ChangeSpeed.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSpeed : Repetidor
+{
+    public float adjust;
+    protected override void AddParticle(GameObject coll)
+    {
+        base.AddParticle(coll);
+        particuleReference.GetComponent<ParticleSystem>().startSpeed = coll.GetComponent<ParticleSystem>().startSpeed * adjust;
+    }
+}
